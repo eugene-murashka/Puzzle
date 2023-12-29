9d65f2275050ff580824ea3f4c583642ca082b97
Yauheni Murashka <eugene.murashka@gmail.com>
2023-12-29 14:50:56 +0300

    Refactor Addition Add Division

--- App/Program.cs
+++ App/Program.cs
@@ -19,11 +19,10 @@ internal class Program
                 break;
             }
 
-            var nam1 = r.Next(2, 10);
-            var nam2 = r.Next(2, 10);
+            IArithmeticOperation ariOper = index % 2 == 0 ? new Addition() : new Division();
 
             Console.WriteLine($"Решите задачу #{++index}");
-            Console.WriteLine($"{nam1} * {nam2} = ?");
+            Console.WriteLine($"{ariOper.GetExpression()} = ?");
 
             var task = new Task<string>(() => Console.ReadLine());
 
@@ -37,7 +36,7 @@ internal class Program
                 break;
             }
 
-            isCorrectAnswer = answer == (nam1 * nam2).ToString();
+            isCorrectAnswer = ariOper.CheckAnswer(answer);
 
             if (isCorrectAnswer)
             {
@@ -45,7 +44,7 @@ internal class Program
             }
             else
             {
-                Console.WriteLine($"Правильный ответ: {nam1 * nam2}");
+                Console.WriteLine($"Правильный ответ: {ariOper.Answer}");
             }
 
             Statistic.Add((isCorrectAnswer, sw.ElapsedMilliseconds));
@@ -57,10 +56,62 @@ internal class Program
         // Статистика среднего времени на ответ
 
         Console.WriteLine("Ваша статистика:\n" +
-            $"Правильные ответы: {Statistic.Count(s => s.Item1)}/{Statistic.Count} - {(float)(Statistic.Count(s => s.Item1))/(float)(Statistic.Count)}%\n" +
+            $"Правильные ответы: {Statistic.Count(s => s.Item1)}/{Statistic.Count} - {(float)(Statistic.Count(s => s.Item1))/(float)(Statistic.Count)*100}%\n" +
             $"Среднее время на ответ: {Statistic.Sum(s => s.Item2)/Statistic.Count/1000.00}s");
 
         Console.WriteLine();
         Console.WriteLine("Спасибо за работу!");
+    }
+
+    public interface IArithmeticOperation
+    {
+        string GetExpression();
+        bool CheckAnswer(string answer);
+        int Answer { get; }
+    }
+
+    internal abstract class ArithmeticOperation
+    {
+        internal ArithmeticOperation()
+        {
+            var r = new Random();
+            nam1 = r.Next(2, 10);
+            nam2 = r.Next(2, 10);
+            product = nam1 * nam2;
+        }
+
+        protected readonly int nam1;
+        protected readonly int nam2;
+        protected readonly int product;
+    }
+
+    internal class Addition : ArithmeticOperation, IArithmeticOperation
+    {
+        public int Answer => product;
+
+        public bool CheckAnswer(string answer)
+        {
+            return answer == (nam1 * nam2).ToString();
+        }
+
+        public string GetExpression()
+        {
+            return $"{nam1} * {nam2}";
+        }
+    }
+
+    internal class Division : ArithmeticOperation, IArithmeticOperation
+    {
+        public int Answer => nam2;
+
+        public bool CheckAnswer(string answer)
+        {
+            return answer == (product / nam1).ToString();
+        }
+
+        public string GetExpression()
+        {
+            return $"{product} / {nam1}";
+        }
     }
 }
