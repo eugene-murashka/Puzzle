3c2ab3f03125e8471e8704e76f55f2b7a1d573dd
Yauheni Murashka <eugene.murashka@gmail.com>
2024-01-01 15:46:56 +0300

    Refactor App

--- App/Program.cs
+++ App/Program.cs
@@ -1,130 +1,162 @@
 ﻿using System.Diagnostics;
+using System.Text;
 
-internal class Program
+internal static class Program
 {
-    private static async Task Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        var statistic = Statistic.Instance;
+        new PuzzleApplication(Encoding.UTF8)
+            .Run();
+    }
+}
 
-        var r = new Random();
-        var index = 0;
-        var isCorrectAnswer = false;
+internal class PuzzleApplication
+{
+    internal PuzzleApplication()
+    {
+    }
 
-        (bool, long) statisticItem; 
+    internal PuzzleApplication(Encoding encoding)
+    {
+        Console.OutputEncoding = encoding;
+    }
 
-        while (true)
+    internal void Run()
+    {
+        string _answer = "";
+        while (_index < _maxOfNumberOfTasks)
         {
-            if(index > 10)
+            _answer = GetAnswer();
+
+            if (_answer == "q")
             {
                 break;
             }
 
-            IArithmeticOperation ariOper = index % 2 == 0 ? new Addition() : new Division();
-
-            Console.WriteLine($"Решите задачу #{++index}");
-            Console.WriteLine($"{ariOper.GetExpression()} = ?");
-
-            var task = new Task<string>(() => Console.ReadLine());
-
-            var sw = Stopwatch.StartNew();
-            task.Start();
-            var answer = await task;
-            sw.Stop();
-
-            if (answer == "q")
-            {
-                break;
-            }
-
-            isCorrectAnswer = ariOper.CheckAnswer(answer);
-
-            if (isCorrectAnswer)
-            {
-                Console.WriteLine("Правильно!");
-            }
-            else
-            {
-                Console.WriteLine($"Правильный ответ: {ariOper.Answer}");
-            }
-
-            statisticItem = (isCorrectAnswer, sw.ElapsedMilliseconds);
-            statistic.Items.Add(statisticItem);
+            MakeAReactionToAnAnswer(_answer);
 
             Console.WriteLine();
         }
+        SayGoodbye();
+    }
 
+    #region private
+    private int _index = 0;
+    private int _maxOfNumberOfTasks = 10;
+    private bool _isCorrectAnswer = false;
+
+    private Statistic _statistic = Statistic.Instance;
+    private (bool, long) _statisticItem;
+    private Stopwatch _stopwatch = new Stopwatch();
+
+    private IArithmeticOperation _ariOper;
+
+    private string GetAnswer()
+    {
+        _ariOper = _index % 2 == 0 ? new Addition() : new Division();
+
+        Console.WriteLine($"Решите задачу #{++_index}");
+        Console.WriteLine($"{_ariOper.GetExpression()} = ?");
+
+        var task = new Task<string>(() => Console.ReadLine());
+
+        _stopwatch.Restart();
+        task.Start();
+        var answer = task.Result;
+        _stopwatch.Stop();
+
+        return answer;
+    }
+    private void MakeAReactionToAnAnswer(string answer)
+    {
+        _isCorrectAnswer = _ariOper.CheckAnswer(answer);
+
+        if (_isCorrectAnswer)
+        {
+            Console.WriteLine("Правильно!");
+        }
+        else
+        {
+            Console.WriteLine($"Правильный ответ: {_ariOper.Answer}");
+        }
+
+        _statisticItem = (_isCorrectAnswer, _stopwatch.ElapsedMilliseconds);
+        _statistic.Items.Add(_statisticItem);
+    }
+    private void SayGoodbye()
+    {
         Console.WriteLine("Ваша статистика:\n" +
-            $"Правильные ответы: {statistic.PosResCount}/{statistic.Count} - {statistic.Grade} баллов\n" +
-            $"Среднее время на ответ: {statistic.AwerageTimer}s");
+            $"Правильные ответы: {_statistic.PosResCount}/{_statistic.Count} - {_statistic.Grade} баллов\n" +
+            $"Среднее время на ответ: {_statistic.AwerageTimer}s");
 
         Console.WriteLine();
         Console.WriteLine("Спасибо за работу!");
     }
+    #endregion
+}
 
-    internal class Statistic
+internal class Statistic
+{
+    internal readonly List<(bool isCorrectAnswer, long timer)> Items = new List<(bool isCorrectAnswer, long timer)>();
+
+    internal int Grade => 100 * Items.Count(s => s.Item1) / Items.Count;
+    internal int Count => Items.Count;
+    internal int PosResCount => Items.Count(s => s.Item1);
+    internal float AwerageTimer => (float)Items.Sum(s => s.Item2) / (float)Items.Count / 1000.00F;
+
+    internal static Statistic Instance => new Statistic();
+
+    private Statistic() { }
+}
+
+public interface IArithmeticOperation
+{
+    string GetExpression();
+    bool CheckAnswer(string answer);
+    int Answer { get; }
+}
+
+internal abstract class ArithmeticOperation
+{
+    internal ArithmeticOperation()
     {
-        internal readonly List<(bool isCorrectAnswer, long timer)> Items = new List<(bool isCorrectAnswer, long timer)>();
-        
-        internal int Grade => 100 * Items.Count(s => s.Item1) / Items.Count;
-        internal int Count => Items.Count;
-        internal int PosResCount => Items.Count(s => s.Item1);
-        internal float AwerageTimer => (float)Items.Sum(s => s.Item2) / (float)Items.Count / 1000.00F;
-        
-        internal static Statistic Instance => new Statistic();
-
-        private Statistic() { }
+        var r = new Random();
+        nam1 = r.Next(2, 10);
+        nam2 = r.Next(2, 10);
+        product = nam1 * nam2;
     }
 
-    public interface IArithmeticOperation
+    protected readonly int nam1;
+    protected readonly int nam2;
+    protected readonly int product;
+}
+
+internal class Addition : ArithmeticOperation, IArithmeticOperation
+{
+    public int Answer => product;
+
+    public bool CheckAnswer(string answer)
     {
-        string GetExpression();
-        bool CheckAnswer(string answer);
-        int Answer { get; }
+        return answer == (nam1 * nam2).ToString();
     }
 
-    internal abstract class ArithmeticOperation
+    public string GetExpression()
     {
-        internal ArithmeticOperation()
-        {
-            var r = new Random();
-            nam1 = r.Next(2, 10);
-            nam2 = r.Next(2, 10);
-            product = nam1 * nam2;
-        }
+        return $"{nam1} * {nam2}";
+    }
+}
 
-        protected readonly int nam1;
-        protected readonly int nam2;
-        protected readonly int product;
+internal class Division : ArithmeticOperation, IArithmeticOperation
+{
+    public int Answer => nam2;
+
+    public bool CheckAnswer(string answer)
+    {
+        return answer == (product / nam1).ToString();
     }
 
-    internal class Addition : ArithmeticOperation, IArithmeticOperation
+    public string GetExpression()
     {
-        public int Answer => product;
-
-        public bool CheckAnswer(string answer)
-        {
-            return answer == (nam1 * nam2).ToString();
-        }
-
-        public string GetExpression()
-        {
-            return $"{nam1} * {nam2}";
-        }
-    }
-
-    internal class Division : ArithmeticOperation, IArithmeticOperation
-    {
-        public int Answer => nam2;
-
-        public bool CheckAnswer(string answer)
-        {
-            return answer == (product / nam1).ToString();
-        }
-
-        public string GetExpression()
-        {
-            return $"{product} / {nam1}";
-        }
+        return $"{product} / {nam1}";
     }
 }
